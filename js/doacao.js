5db47b54aa98f47709cc0e27befa3ba56433975d
Messias Nunesmaia <messias.lsn@gmail.com>
2023-05-18 23:54:16 -0300

    Criação de animação ao copiar CNPJ

--- js/doacao.js
+++ js/doacao.js
@@ -1,4 +1,6 @@
 function copyText() {
+
+  // Pega o texto e coloca na área de transferência
   var text = document.getElementById('text-to-copy');
   var range = document.createRange();
   range.selectNode(text);
@@ -7,12 +9,20 @@ function copyText() {
   document.execCommand('copy');
   window.getSelection().removeAllRanges();
 
+  // Pega o botão, troca as classes e seu texto
   var button = document.getElementById("btn-click-to-copy");
+  button.classList.remove("btn-outline-primary");
+  button.classList.add("btn-secondary");
+  button.classList.add("text-white");
   var oldText = button.innerHTML;
   var newText = "Copiado !";
   button.innerHTML = newText;
 
-  setTimeout(function() {
+  // Troca as classes e texto do botão novamente, após 2000 millisegundos
+  setTimeout(function () {
+    button.classList.remove("btn-secondary");
+    button.classList.remove("text-white");
+    button.classList.add("btn-outline-primary");
     button.innerHTML = oldText;
   }, 2000);
 }
