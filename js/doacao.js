b3211f381d2850ab33a3402cc879af57dce26a49
Messias Nunesmaia <messias.lsn@gmail.com>
2023-05-18 23:44:01 -0300

    Troca de texto ao copiar CNPJ

--- js/doacao.js
+++ js/doacao.js
@@ -1,4 +1,3 @@
-
 function copyText() {
   var text = document.getElementById('text-to-copy');
   var range = document.createRange();
@@ -7,5 +6,13 @@ function copyText() {
   window.getSelection().addRange(range);
   document.execCommand('copy');
   window.getSelection().removeAllRanges();
-}
 
+  var button = document.getElementById("btn-click-to-copy");
+  var oldText = button.innerHTML;
+  var newText = "Copiado !";
+  button.innerHTML = newText;
+
+  setTimeout(function() {
+    button.innerHTML = oldText;
+  }, 2000);
+}
