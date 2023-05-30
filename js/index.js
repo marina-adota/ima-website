a14bd657a7cbb6be283e530ddfe630dbe7d045fa
Messias Nunesmaia <messias.lsn@gmail.com>
2023-05-30 07:33:54 -0300

    Responsividade de slide na pagina principal

--- js/index.js
+++ js/index.js
@@ -1,31 +1,31 @@
 var titles = [
-    "Cada animal tem uma história,<br>ajude a escrever um final feliz",
-    "Adote, salve uma vida e ganhe um amigo fiel.", 
-    "Adote amor, salve vidas.",
-    "Com amor e cuidado, transformamos a vida de animais abandonados.", 
-    "Nossa missão é proteger e cuidar dos animais, um de cada vez."
+  "Adoção de cachorros em Salvador",
+  "Cada animal tem uma história,<br>ajude a escrever um final feliz",
+  "Adote, salve uma vida e ganhe um amigo fiel",
+  "Adote amor, salve vidas",
+  "Com amor e cuidado, transformamos a vida de animais abandonados",
+  "Nossa missão é proteger e cuidar dos animais, um de cada vez"
 ];
 var currentIndex = 0;
 
 function changeTitle(elementIds) {
-    elementIds.forEach(function (elementId) {
-      var titleElement = document.getElementById(elementId);
-  
-      titleElement.style.opacity = 0;
-  
+  elementIds.forEach(function (elementId) {
+    var titleElement = document.getElementById(elementId);
+
+    titleElement.style.opacity = 0;
+
+    setTimeout(function () {
+      titleElement.innerHTML = titles[currentIndex];
+
+      titleElement.style.opacity = 1;
+
+      currentIndex = (currentIndex + 1) % titles.length;
+
       setTimeout(function () {
-        titleElement.innerHTML = titles[currentIndex];
-  
-        titleElement.style.opacity = 1;
-  
-        currentIndex = (currentIndex + 1) % titles.length;
-  
-        setTimeout(function () {
-          changeTitle(elementIds);
-        }, 6000);
-      }, 700);
-    });
-  }
-  
-  changeTitle(["title", "lastMessage"]);
-  
+        changeTitle(elementIds);
+      }, 6000);
+    }, 700);
+  });
+}
+
+changeTitle(["title", "lastMessage"]);
